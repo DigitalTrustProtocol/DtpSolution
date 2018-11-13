c804ba7bd9e32c353c3a21ac4a1b1a171e7692e3
Carsten Keutmann <keutmann@yahoo.com>
2018-11-13 13:44:20 +0100

    Added TrustPackage model to support multiple packages for one Trust.

---
+++ DtpServer/Pages/Packages/Details.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DtpCore.Model;
+using DtpCore.Repository;
+using DtpCore.Collections.Generic;
+
+namespace DtpServer.Pages.Packages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DtpCore.Repository.TrustDBContext _context;
+
+        public DetailsModel(DtpCore.Repository.TrustDBContext context)
+        {
+            _context = context;
+        }
+
+        public Package Package { get; set; }
+        public PaginatedList<Trust> Trusts { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Package = await _context.Packages.FirstOrDefaultAsync(m => m.DatabaseID == id);
+
+            if (Package == null)
+            {
+                return NotFound();
+            }
+
+            var query = _context.Trusts.Where(p => p.PackageDatabaseID == id).OrderBy(p=> p.Created);
+            Trusts = await PaginatedList<Trust>.CreateAsync(query, 1, 0); // PageSize = 0 is unlimited
+
+
+            return Page();
+        }
+    }
+}
---
+++ DtpServer/Pages/Packages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DtpCore.Model;
+using DtpCore.Repository;
+
+namespace DtpServer.Pages.Packages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DtpCore.Repository.TrustDBContext _context;
+
+        public IndexModel(DtpCore.Repository.TrustDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Package> Package { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Package = await _context.Packages.ToListAsync();
+        }
+    }
+}
